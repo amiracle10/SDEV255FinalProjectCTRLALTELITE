bc72bcf11a553286a280e42d8749284013611159
amiracle10 <amiracle10@ivytech.edu>
2025-04-21 17:18:45 -0500

    add github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/SDEV_255_Final_Project_Ctrl-Alt-Elite/",
+  base: "/SDEV255FinalProjectCTRLALTELITE/",
   plugins: [react()],
   root: './frontend', 
   css: {
